77614095ddebfbf296346dd35970edf6e58f038e
Naufal Zakly Santoso <72109817+naufalzakly@users.noreply.github.com>
2023-05-06 08:49:27 +0700

    update

---
+++ netkitchen/resources/views/partial_template/navbar.blade.php
@@ -0,0 +1,31 @@
+<nav class="main-nav">
+    <!-- ***** Logo Start ***** -->
+    <a href="index.html" class="logo">
+        <img src="images/klassy-logo.png" align="klassy cafe html template">
+    </a>
+    <!-- ***** Logo End ***** -->
+    <!-- ***** Menu Start ***** -->
+    <ul class="nav">
+        <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+        <li class="scroll-to-section"><a href="#about">About</a></li>
+           
+    <!-- 
+        <li class="submenu">
+            <a href="javascript:;">Drop Down</a>
+            <ul>
+                <li><a href="#">Drop Down Page 1</a></li>
+                <li><a href="#">Drop Down Page 2</a></li>
+                <li><a href="#">Drop Down Page 3</a></li>
+            </ul>
+        </li>
+    -->
+        <li class="scroll-to-section"><a href="#menu">Menu</a></li>
+        <!-- <li class=""><a rel="sponsored" href="https://templatemo.com" target="_blank">External URL</a></li> -->
+        <li class="scroll-to-section"><a href="#reservation">Contact Us</a></li> 
+        <li class="scroll-to-section"><a href="login">Login</a></li> 
+    </ul>        
+    <a class='menu-trigger'>
+        <span>Menu</span>
+    </a>
+    <!-- ***** Menu End ***** -->
+</nav>
